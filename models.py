d0fe12d5f3e3386313c7bae2046c84b9f5b6cc24
Alexander Lyotov <sippie84@gmail.com>
2022-11-04 21:03:56 +0300

    feature/Recipe and Ingredient models

--- models.py
+++ models.py
@@ -4,25 +4,26 @@ from sqlalchemy.orm import relationship
 from database import Base
 
 
+association_table = Table(
+    "recipe_ingredients",
+    Base.metadata,
+    Column("recipe_id", ForeignKey("recipes.id"), primary_key=True),
+    Column("ingredient_id", ForeignKey("ingredients.id"), primary_key=True)
+)
+
+
 class Recipe(Base):
-    __tablename__ = 'recipe_table'
+    __tablename__ = 'recipes'
     id = Column(Integer, primary_key=True, index=True)
     dish_name = Column(String, index=True)
     views_number = Column(Integer)
     cooking_time = Column(Integer)
     description = Column(String)
-    ingredients = relationship("Ingredient", )
+    ingredients = relationship("Ingredient", secondary='recipe_ingredients', back_populates='recipes')
 
 
 class Ingredient(Base):
-    __tablename__ = "ingredient_table"
+    __tablename__ = "ingredients"
     id = Column(Integer, primary_key=True)
     name = Column(String)
-
-
-association_table = Table(
-    "association_table",
-    Base.metadata,
-    Column("recipe_id", ForeignKey("recipe_table.id"),
-    Column("ingredient_id", ForeignKey("ingredient_table.id")))
-)
+    recipes = relationship("Recipe", secondary="recipe_ingredients", back_populates='ingredients')
